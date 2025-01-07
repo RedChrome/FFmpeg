0be69dbf00bb3307ad7d7e5de7f6d50a89ad09e4
Alex <redchrome@gmx.de>
2025-01-07 18:06:49 +0100

    Update build.sh
    Uncommenting configure

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ DLL_EXPORT_PATH="/mnt/c/Users/Alex/Documents/python/svcap/svcap/dlls"
 LIB_EXPORT_PATH="/mnt/c/Users/Alex/Documents/python/svcap/lib"
 
 # This one works, but disabled to not need to run it every time ...
-#./configure --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32- --disable-static --enable-shared --shlibdir=build --extra-ldflags="-static" --extra-cflags="-lstdc++" --enable-version3
+./configure --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32- --disable-static --enable-shared --shlibdir=build --extra-ldflags="-static" --extra-cflags="-lstdc++" --enable-version3
 
 make -j 15
 
